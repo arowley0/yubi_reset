978fdfdb450d9c9d2c1a653bab4630e7a9029f51
Andi Rowley <redacted@c9h.org>
2017-03-22 17:56:19 +0000

    Adding script file

---
+++ reset_applet_force.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#run this file with the command gpg-connect-agent -r reset_applet_force.sh
+#enter the wrong pin and admin pin multiple times
+/hex
+scd serialno
+scd apdu 00 20 00 81 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 81 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 81 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 81 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 83 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 83 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 83 08 40 40 40 40 40 40 40 40
+scd apdu 00 20 00 83 08 40 40 40 40 40 40 40 40
+#Terminate and reactivate card
+scd apdu 00 e6 00 00
+scd apdu 00 44 00 00
+/echo Card has been successfully reset.
+
